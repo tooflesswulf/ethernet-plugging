ab665997c1f1aa7da1e42e9380d5b938c3af9cc0
YiqiWang <yiqiw2@andrew.cmu.edu>
2026-06-02 10:17:38 -0400

    update eval

---
+++ shells/eval.sh
@@ -0,0 +1,4 @@
+
+python agent/eval/eval.py \
+    --dataset_dir '/home/atkesonlab4/Desktop/YiqiProject/100%_Project/dataset' \
+    --ckpt_dir '/home/atkesonlab4/Desktop/YiqiProject/100%_Project/ckpts' 
