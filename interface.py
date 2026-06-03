a079828d9c8650032faba39b5172e405843f837e
tooflesswulf <tooflesswulf0325@gmail.com>
2026-06-03 14:27:43 -0400

    refactor + hope I ddint break anything

--- interface.py
+++ interface.py
@@ -14,41 +14,67 @@ class DualSenseInterface:
         self.dualsense.start_control()
 
         self.targ_pose = np.array(start_pose)
+        self.targ_zforce = 0.
         self.speed = np.r_[xyzspeed, xyzspeed, xyzspeed, rpyspeed, rpyspeed, rpyspeed]
 
     @property
     def target_pose(self):
         return self.targ_pose
+    
+    @property
+    def target_zforce(self):
+        return self.targ_zforce
 
-    def update(self, dt):
-        act = self.dualsense.input2action()
-        if act is None:
-            print('Act is None, skipping update')
-            return -1
-        delta = act['right_delta']
-        if act['right_gripper']:
-            self.gripper_state = 1 - self.gripper_state
-        if act['toggle_zforce']:
-            self.adaptive_mode = not self.adaptive_mode
-
+    def flip_actions(self, act):
         # Manual flips
         tr = np.array([
             [1, 0, 0],
             [0, -1, 0],
             [0, 0, 1],
         ])
+        delta = act['right_delta']
+        delta[:3] = tr @ delta[:3]
+
+        # Manual flip rotations
+        drx, dry, drz = delta[3:]
+        delta[3:] = dry, drx, -drz
+        return delta
+
+    def update(self, dt):
+        act = self.dualsense.input2action()
+        if act is None:
+            print('Act is None, skipping update')
+            return -1
+        if act['right_gripper']:
+            self.gripper_state = 1 - self.gripper_state
+        if act['toggle_zforce']:
+            self.adaptive_mode = not self.adaptive_mode
+
+        self.flip_actions(act)
+        if self.adaptive_mode:
+            pass # TODO: implement adaptive mode
+        else:
+            self.update_pos_mode(act, dt)
+
+    def update_pos_mode(self, act, dt):
+        delta = act['right_delta']
 
         # Position: simple addition
-        dpos = tr @ delta[:3] * self.speed[:3] * dt
+        dpos = delta[:3] * self.speed[:3] * dt
         self.targ_pose[:3] += dpos
 
         # Orientation: compose delta Euler (ZYX) onto current rotation vector
         drx, dry, drz = delta[3:] * self.speed[3:] * dt
-
-        # Manually flip rotations
-        drx, dry, drz = dry, drx, -drz
-
         R_cur = R.from_rotvec(self.targ_pose[3:])
         R_delta = R.from_euler('ZYX', [drz, dry, drx])
         self.targ_pose[3:] = (R_cur * R_delta).as_rotvec()
-        return 0
+
+    def activate_adaptive_mode(self):
+        self.targ_zforce = self.latest_obs['state']['force'].z
+
+    def deactivate_adaptive_mode(self):
+        self.targ_pose = self.latest_obs['state']['pose']
+        self.targ_zforce = 0.
+
+    def store_obs(self, obs):
+        self.latest_obs = obs
