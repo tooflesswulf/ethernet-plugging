f5a3cdf874f4e1cf2a80528c5d1126aaf5144973
Albert Xu <albertx@andrew.cmu.edu>
2026-06-10 19:55:41 -0400

    sequence.py got torn up :)

--- agent/dataset/sequence.py
+++ agent/dataset/sequence.py
@@ -1,88 +1,25 @@
 
+from collections import namedtuple
 from PIL import Image
 from tqdm import tqdm
-import os
-import numpy as np
-from collections import namedtuple
+from scipy.spatial.transform import Rotation as R, RigidTransform as Tf
 from einops import rearrange, repeat
+import numpy as np
 import torch
+import pathlib
+import os
 # from agent.utils.utils import get_chunk_actions
 
-Batch = namedtuple("Batch", "actions conditions")
-
-
-def pose2actions(poses, grippers, traj_lengths):
-    poses, grippers = poses[:sum(traj_lengths)], grippers[:sum(traj_lengths)]
-    # compute delta actions between poses, add absolute gripper length
-    actions = []
-    offset = 0
-    for traj_length in traj_lengths:
-        start, end = offset, offset + traj_length
-        ep_poses = poses[start:end]
-        ep_poses_shifted = np.concatenate([poses[start + 1:end], poses[end - 1:end]])
-
-        # delta action with absolute gripper width as action
-        ep_actions = ep_poses_shifted - ep_poses
-        shifted_grippers = np.concatenate([grippers[start + 1:end], grippers[end - 1:end]])[:, None]
-        ep_actions = np.concatenate([ep_actions, shifted_grippers], axis=-1)
-        actions = ep_actions if len(actions) == 0 else np.concatenate([actions, ep_actions])
-
-        # increase counter
-        offset += traj_length
-    assert len(actions) == len(poses)
-    return actions
+DataBatch = namedtuple('DataBatch', ['actions', 'conditions'])
 
 
 def get_images(dir_path, total_num_steps=None, img_size=128):
     N = len(os.listdir(dir_path)) if total_num_steps is None else min(len(os.listdir(dir_path)), total_num_steps)
-    images = [np.array(Image.open(os.path.join(dir_path, f'{i}.png')).resize(
-        (img_size, img_size))) for i in tqdm(range(N), desc="loading images to RAM")]
+    images = []
+    for i in tqdm(range(N), desc="loading images to RAM"):
+        img = Image.open(os.path.join(dir_path, f'{i}.png')).resize((img_size, img_size))
+        images.append(np.array(img))
     return np.array(images)
-
-
-def normalize(arr: np.ndarray, min_val=None, max_val=None) -> np.ndarray:
-    """
-    Normalize a numpy array to the range [-1, 1] using min/max scaling.
-    """
-    # CAVEAT: normalize by dimension
-    min_val = arr.min(0) if min_val is None else min_val
-    max_val = arr.max(0) if max_val is None else max_val
-
-    return 2 * (arr - min_val) / (max_val - min_val) - 1
-
-
-def get_chunk_actions_stats(states, horizon, traj_lengths):
-    def _ep_actions(ep_states, horizon):
-        ep_actions = []
-        indices = np.arange(0, len(ep_states) - horizon + 1, dtype=int)
-        for index in indices:
-            meta_pose = ep_states[index]
-            future_poses = ep_states[index + 1:index + horizon + 1]
-            actions = future_poses - meta_pose
-            # replace gripper with absolute gripper width
-            actions[:, -1] = future_poses[:, -1]
-            paddings = horizon - len(actions)
-            if paddings > 0:
-                padded = np.concatenate([actions[-1:]] * paddings)
-                actions = np.concatenate([actions, padded])
-            ep_actions.append(actions)
-        return np.array(ep_actions)
-    ep_start = 0
-    ep_actions, all_actions = [], []
-    for traj_length in traj_lengths:
-        ep_states = states[ep_start: ep_start + traj_length]
-        out = _ep_actions(ep_states, horizon)
-        # print( f"{out.reshape(-1, 7)[::16, -1]}" )
-        ep_actions.append(out)
-        ep_start += traj_length
-        if len(all_actions) == 0:
-            all_actions = out
-        else:
-            all_actions = np.concatenate([all_actions, out])
-    all_actions = all_actions.reshape(-1, all_actions.shape[-1])
-    max_, min_ = all_actions.max(0), all_actions.min(0)
-    # assert False
-    return max_, min_
 
 
 class StitchedSequenceDataset(torch.utils.data.Dataset):
@@ -97,7 +34,6 @@ class StitchedSequenceDataset(torch.utils.data.Dataset):
         Episode IDs (determined based on traj_lengths):  [----------   1  ----------][----------   2  ---------] ... [----------   N  ---------]
 
     Each sample is a namedtuple of (1) chunked actions and (2) a list (obs timesteps) of dictionary with keys states and images.
-
     """
 
     def __init__(
@@ -107,48 +43,34 @@ class StitchedSequenceDataset(torch.utils.data.Dataset):
         cond_steps=1,
         img_cond_steps=1,
         max_n_episodes=10000,
+        obs_fields=['pose', 'gripper_width'],
+        transform=None,
         device="cuda:0",
     ):
-        assert (
-            img_cond_steps <= cond_steps
-        ), "consider using more cond_steps than img_cond_steps"
+        assert img_cond_steps <= cond_steps, 'consider using more cond_steps than img_cond_steps'
         self.horizon_steps = horizon_steps
         self.cond_steps = cond_steps  # states (proprio, etc.)
         self.img_cond_steps = img_cond_steps
         self.device = device
+        self.transform = transform
 
         self.max_n_episodes = max_n_episodes
-        self.dataset_path = dataset_path
+        self.dataset_path = pathlib.Path(dataset_path)
 
-        # Load dataset to device specified
-        img_dir = os.path.join(dataset_path, 'images')
-        state_path = os.path.join(dataset_path, 'states.npz')
+        # Load dataset to device
+        img_dir = self.dataset_path / 'images'
+        state_path = self.dataset_path / 'states.npz'
         dataset = np.load(state_path, allow_pickle=False)  # only np arrays
-        traj_lengths = dataset["traj_length"][:max_n_episodes]  # 1-D array
+        traj_lengths = dataset['traj_length'][:max_n_episodes]  # 1-D array
         total_num_steps = np.sum(traj_lengths)
-        # actions = pose2actions(dataset['actual_pose'],  dataset['gripper_width'], traj_lengths)
-        states = np.concatenate([dataset['pose'], dataset['gripper_width'][:, None]], axis=-1)
+        states = np.c_[*[dataset[key] for key in obs_fields]]  # Concat along columns, (total_num_steps, obs_dim)
 
-        action_max, action_min = get_chunk_actions_stats(states[:total_num_steps], horizon_steps, traj_lengths)
-        self.action_max = torch.from_numpy(action_max).float().to(device)
-        self.action_min = torch.from_numpy(action_min).float().to(device)
-        self.state_max, self.state_min = torch.from_numpy(states[:total_num_steps].max(0)).float().to(
-            device), torch.from_numpy(states[:total_num_steps].min(0)).float().to(device)
         # Set up indices for sampling
         self.indices = self.make_indices(traj_lengths, horizon_steps)
 
         # Extract states and actions up to max_n_episodes
-        self.states = (
-            torch.from_numpy(states[:total_num_steps]).float().to(device)
-            # torch.from_numpy(states[:total_num_steps]).float().to(device)
-        )  # (total_num_steps, obs_dim)
-        # self.actions = (
-        #     torch.from_numpy(normalize(actions[:total_num_steps])).float().to(device)
-        # )  # (total_num_steps, action_dim)
-
-        self.images = torch.from_numpy(get_images(img_dir, total_num_steps)).to(
-            device
-        )  # (total_num_steps, H, W, C)
+        self.states = torch.from_numpy(states[:total_num_steps]).float().to(device)  # (total_num_steps, obs_dim)
+        self.images = torch.from_numpy(get_images(img_dir, total_num_steps)).to(device)  # (total_num_steps, H, W, C)
 
     def __getitem__(self, idx):
         """
@@ -198,13 +120,15 @@ class StitchedSequenceDataset(torch.utils.data.Dataset):
         )  # img_cond_steps x H x W x C
 
         conditions["rgb"] = rearrange(images, ' T H W C -> T C H W') / 255.0  # - 0.5
-        batch = Batch(actions, conditions)
+        batch = DataBatch(actions, conditions)
         return batch
 
     def make_indices(self, traj_lengths, horizon_steps):
         """
         makes indices for sampling from dataset;
         each index maps to a datapoint, also save the number of steps before it within the same trajectory
+
+        Returns list[(start_index, num_before_start, traj_end_index)], where
         """
         indices = []
         cur_traj_index = 0
