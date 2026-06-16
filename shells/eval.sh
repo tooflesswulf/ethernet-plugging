83fa35996a190cea9de49f04c46dab237a1acc14
YiqiWang <yiqiw2@andrew.cmu.edu>
2026-06-16 14:09:35 -0400

    changes (not important)

--- shells/eval.sh
+++ shells/eval.sh
@@ -1,4 +1,4 @@
 
 python agent/eval/eval.py \
     --ckpt '/home/atkesonlab4/Desktop/YiqiProject/100%_Project/ckpts/yiqi_pluginv2_local_delta/ckpt_final.pth' \
-    --log_dir './logs_plugin_local_delta/'
+    --log_dir './logs_pluginv2_local_delta_50/'
