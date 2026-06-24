03644645c039960180619681601533466618e799
Albert Xu <tooflesswulf0325@gmail.com>
2026-06-23 22:35:28 -0400

    Albert/eval rl (#11)
    * Implemented realtime_chunking. Authored by: claude
    
    * debug statements
    
    * stuff and things
    
    * fix gripper error if initially gripping
    
    * default action freq -> 10
    
    * fps=20
    
    * fix gripper homing bug (again?)
    
    * commited by yiqi, June18, 9:30am
    
    * Implement DDIM (faster inference) untested.
    
    * debug realtime chunking notebook
    
    * make robot utils
    
    * 1st attempt at teleop-rl script
    
    * Somewhat feasible rl correction
    
    * 3/10 success rate pickup-plug-unplug-drop
    
    * bugfix in iface-as-residual
    
    ---------
    
    Co-authored-by: YiqiWang <yiqiw2@andrew.cmu.edu>

--- shells/eval.sh
+++ shells/eval.sh
@@ -1,4 +1,5 @@
 
 python agent/eval/eval.py \
-    --ckpt '/home/atkesonlab4/Desktop/YiqiProject/100%_Project/ckpts/yiqi_pluginv2_local_delta/ckpt_ep_150.pth' \
-    --log_dir './logs_pluginv2_local_delta_50/'
+    --ckpt '/home/atkesonlab4/Desktop/YiqiProject/100%_Project/ckpts/ethernet_plugin_unplug/h16/ckpt_ep_190.pth' \
+    --log_dir './logs_plugin_unplug_50_rtc/' \
+    --mode realtime
