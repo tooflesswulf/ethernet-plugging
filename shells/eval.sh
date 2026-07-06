40ee13adc925b75f5840e3f9e30067b00f763230
YiqiWang <yiqiw2@andrew.cmu.edu>
2026-07-06 13:46:02 -0400

    shell strings

--- shells/eval.sh
+++ shells/eval.sh
@@ -1,5 +1,5 @@
-
-python agent/eval/eval.py \
+#!/bin/bash
+python agent/eval/eval_realtime.py \
     --ckpt '/home/atkesonlab4/Desktop/YiqiProject/100%_Project/ckpts/ethernet_plugin_unplug_75/h16/ckpt_final.pth' \
-    --log_dir './logs_plugin_unplug_75_rtc/' \
-    --mode realtime
+    --log_dir './logs-eval/75-rtc' \
+    # --mode realtime
