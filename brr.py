e55586c900b5f39eceecd160c1b9fe201421d418
YiqiWang <yiqiw2@andrew.cmu.edu>
2026-09-18 17:51:33 -0400

    happy with how this feels

--- brr.py
+++ brr.py
@@ -8,7 +8,7 @@ ctrl.setCustomScriptFile('rtde_control-1.6.5-frictionfix.script')
 recv = rtde_receive.RTDEReceiveInterface('192.168.0.100')
 
 VIS = [.9, .9, .8, .9, .9, .9]
-COU = [.8, .8, .7, .8, .8, .8]
+COU = [.9, .8, .8, .7, .8, 1.]
 
 stamps = []
 joints = []
@@ -16,12 +16,13 @@ joints = []
 try:
     while True:
         t0 = ctrl.initPeriod()
-        ctrl.directTorque([0, 0, 0, 0, 0, 0])
+        ctrl.directTorque([0, 0, 0, 0, 0, 0], VIS, COU)
         q = recv.getActualQ()
         print(q, end='\r')
         stamps.append(time.perf_counter())
         joints.append(q)
         ctrl.waitPeriod(t0)
 except KeyboardInterrupt:
+    ctrl.stopScript()
     print('saving...')
-    np.savez('brr-log.npz', times=stamps, joints=joints)
+    # np.savez('brr-log.npz', times=stamps, joints=joints)
